887e7b54777777d9e3b8820425cf4966d223a0f3
NavidKJalali <navid.jalali@prezi.com>
2021-09-28 04:47:34 +0200

    ✨ add some syntax

--- src/main/scala/fp/Syntax.scala
+++ src/main/scala/fp/Syntax.scala
@@ -1,7 +1,26 @@
 package fp
 
 object Syntax {
-  implicit class TraversableApplicativeOps[F[_]: Traversable, G[_]: Applicative, A](private val in: F[G[A]]) {
-    def sequenceA: G[F[A]] = implicitly[Traversable[F]].traverse(in)(identity)
+  implicit class TraversableApplicativeOps[F[_] : Traversable, G[_] : Applicative, A](private val in: F[G[A]]) {
+    def sequenceA: G[F[A]] = in.traverse(identity)
+  }
+
+  implicit class TraversableOps[F[_] : Traversable, A](private val in: F[A]) {
+    def traverse[G[_] : Applicative, B](f: A => G[B]): G[F[B]] =
+      implicitly[Traversable[F]].traverse(in)(f)
+  }
+
+  implicit class FunctorOps[F[_] : Functor, A](private val fa: F[A]) {
+    def map[B](f: A => B): F[B] = implicitly[Functor[F]].map(f)(fa)
+  }
+
+  implicit class ApplicativeOps[F[_] : Applicative, A](private val fa: F[A]) {
+    private val applicative = implicitly[Applicative[F]]
+
+    def product[B](fb: F[B]): F[(A, B)] = applicative.product(fa, fb)
+
+    def ap[B](ff: F[A => B]): F[B] = applicative.ap(ff)(fa)
+
+    def map[B](ff: A => B): F[B] = applicative.map(ff)(fa)
   }
 }
